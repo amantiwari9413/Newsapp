4d298cf066d072c114fbeca7d811b96026f3f046
amantiwari9413 <tiwariaman9413@gamil.com>
2024-02-22 21:32:37 +0530

    small change in businees

--- src/componet/Business.jsx
+++ src/componet/Business.jsx
@@ -4,6 +4,7 @@ export default function Business() {
   return (
    <>
    <App contury={"in"} category={"business"} newstype={"top-headlines"}/>
+   {console.log("hello world i am in business catogory")}
    </>
   )
 }
